327bed86fa9c768e11d1c55e73a8e702d3756e3f
gurusamyp <gurumprp@gmail.com>
2025-12-12 14:45:26 +0530

    symbol js

--- Js_basics/symbols.js
+++ Js_basics/symbols.js
@@ -12,3 +12,31 @@ console.log(id1);
 
 // to convers the simbol to string
 console.log(id1.toString());
+
+
+let user = {
+    name: "Guru",
+    location : "munnar",
+}
+
+let id  = Symbol("id_N");
+user[id] = id;
+console.log(user[id]);
+
+// symbols are skipped in for..in loop
+
+for(let key in user){
+    console.log(`${key} : ${user[key]}`); //name : Guru , location : munnar ----No symbol is display. 
+}
+
+//Global symbol....registry.
+
+let idNew = Symbol.for("new_ID");
+
+let idNewAgain = Symbol.for('new_ID');
+
+console.log(idNew == idNewAgain); /*true -- because a symboll is created if there is no symboal in that name, 
+                                   otherwise the stored symbol value is provided as ouptut */
+
+console.log(Symbol.keyFor(idNew)); // new_ID
+console.log(Symbol.keyFor(idNewAgain)); // new_ID
