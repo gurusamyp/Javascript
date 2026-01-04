42c6701d2945171f6d58299596035a14bee3547e
gurusamyp <gurumprp@gmail.com>
2026-01-04 09:04:09 +0530

    for learn git

--- gitLearn/app.js
+++ gitLearn/app.js
@@ -1,2 +1,10 @@
 const price = 100;
-console.log(`Standard Price: $${price}`);
+
+// New logic added in this branch
+function getDiscount(userType) {
+    if (userType === 'gold') return 0.20; // 20% off
+    return 0;
+}
+
+const finalPrice = price * (1 - getDiscount('gold'));
+console.log(`Discounted Price: $${finalPrice}`);
