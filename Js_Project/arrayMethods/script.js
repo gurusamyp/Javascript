2ae4cf29d250d2edbecfce7db908e442427b8455
gurusamyp <gurumprp@gmail.com>
2026-01-21 21:29:44 +0530

    not yet completed methods

--- Js_Project/arrayMethods/script.js
+++ Js_Project/arrayMethods/script.js
@@ -14,24 +14,47 @@ document.getElementById('loadBtn').addEventListener('click', async () => {
     displayUsers(users);
 });
 
+//to show user's individual data
+
+
+//to close user's individual data screen
+document.getElementById('close').onclick = (e)=>{
+    document.getElementById('userDetails').classList.add('hidden');
+}
+
+
+
 //Display data in HTML
 
 function displayUsers(users){
     const tbody = document.getElementById('tableBody');
     tbody.innerHTML = '';
-
+/* forEach method */
     users.forEach(user => {  // forEach dont return values so we use it when you don’t need a return value.
         const tr = document.createElement('tr');
+        //set id value for each row
+        tr.setAttribute('id', user.id);
 
+        //populate each row with data
         tr.innerHTML = `
-        <td>${user.id}</td>
-        <td>${user.firstName} ${user.lastName}</td>
-        <td>${user.age}</td>
-        <td>${user.address.city}</td>
-        <td>${user.company.name}</td>
-        <td>${user.ssn}</td>
-    `;
+            <td>${user.id}</td>
+            <td class ='userCard'> 
+                <span>&#9658;</span>
+                ${user.firstName} ${user.lastName}
+            </td>
+            <td>${user.age}</td>
+            <td>${user.address.city}</td>
+            <td>${user.company.name}</td>
+            <td>${user.ssn}</td>
+        `;
 
     tbody.appendChild(tr);
     });
 }
+
+
+
+function showUserDetails(id){
+        
+    
+    }
